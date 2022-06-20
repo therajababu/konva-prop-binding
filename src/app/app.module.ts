07c8b9d4debcaa5a3e4e91e26e4c91d402fb06a5
Raja Babu <rajababu1259@gmail.com>
2022-06-20 11:58:43 +0530

    new component canvas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import KonvaModule
+import { KonvaModule } from "ng2-konva";
+import { CanvasComponent } from './canvas/canvas.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KonvaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
